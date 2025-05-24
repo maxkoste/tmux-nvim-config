7e1cefc1c3c97356f46e5c5ef988b46705580a46
Max Koste <60856309+maxkoste@users.noreply.github.com>
2025-05-24 18:07:00 +0200

    now gradle projects work and have a :Run method in nvim to run the code

--- nvim/ftplugin/java.lua
+++ nvim/ftplugin/java.lua
@@ -1,6 +1,28 @@
 vim.api.nvim_create_user_command("Run", function()
-  local current_file = vim.fn.expand("%:p")
-  local main_class = current_file:gsub(".*/src/main/java/", ""):gsub("%.java", ""):gsub("/", ".")
+  local function is_gradle_project()
+    return vim.fn.filereadable(vim.fn.getcwd() .. "/build.gradle") == 1
+  end
 
-  vim.cmd("split | terminal mvn compile exec:java -Dexec.mainClass=" .. main_class)
+  local function is_maven_project()
+    return vim.fn.filereadable(vim.fn.getcwd() .. "/pom.xml") == 1
+  end
+
+  local function get_main_class()
+    local current_file = vim.fn.expand("%:p")
+    return current_file:gsub(".*/src/main/java/", ""):gsub("%.java", ""):gsub("/", ".")
+  end
+
+  local main_class = get_main_class()
+  local command = nil
+
+  if is_gradle_project() then
+    command = "gradle run --args='" .. main_class .. "'"
+  elseif is_maven_project() then
+    command = "mvn compile exec:java -Dexec.mainClass=" .. main_class
+  else
+    vim.notify("No build.gradle or pom.xml found in project root.", vim.log.levels.ERROR)
+    return
+  end
+
+  vim.cmd("split | terminal " .. command)
 end, {})
